39a26c98dc7ca86376fcfe3b89d77f23bc22938a
adalid2608 <adalid.islas26@gmail.com>
2023-11-12 19:39:41 -0600

    Add: Implementación de menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,28 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import {MatTabsModule} from '@angular/material/tabs';
+import { MenuComponent } from "./componentes/menu/menu.component";
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatTabsModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatSelectModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatTabsModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatSelectModule,
+        BrowserAnimationsModule,
+        MenuComponent
+    ]
 })
 export class AppModule { }
