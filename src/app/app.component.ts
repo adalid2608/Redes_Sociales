7f35c6e7c00e4e829839daa7a1dbe4a53db20f36
Arely2409 <arely.aguilarfarias@utxicotepec.edu.mx>
2023-11-14 18:32:23 -0600

    Add: Implementacion de red social TikTok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   mostrarInstagram : boolean = true
   mostrarMesseger : boolean = false
   mostrarWhatsApp : boolean = false
+  mostrarTikTok : boolean = false
   
   constructor(){}
 
@@ -17,18 +18,28 @@ export class AppComponent {
   mostrarRedInstagram(){
     this.mostrarInstagram = true
     this.mostrarMesseger = false
+    this.mostrarTikTok = false
     this.mostrarWhatsApp = false
   }
 
   mostrarRedSocialMessenger(){
     this.mostrarInstagram = false
     this.mostrarMesseger = true
+    this.mostrarTikTok = false
+    this.mostrarWhatsApp = false
+  }
+  
+  mostrarRedSocialTikTok(){
+    this.mostrarInstagram = false
+    this.mostrarMesseger = false
+    this.mostrarTikTok = true
     this.mostrarWhatsApp = false
   }
 
   mostrarRedSocialWhatsApp(){
     this.mostrarInstagram = false
     this.mostrarMesseger = false
+    this.mostrarTikTok = false
     this.mostrarWhatsApp = true
   }
 
---
+++ src/app/core/interfaces/IChat.ts
@@ -0,0 +1,8 @@
+export interface IChat{
+    userImage: string,
+    userName: string,
+    message: string, 
+    status: boolean,
+    fecha: string,
+    numMensajes: number
+}
